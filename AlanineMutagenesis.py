d6a535d00c8fb9d952b047ff448b65bb34d36163
mariaangelesjs <mariaangelesjs@gmail.com>
2019-08-30 00:43:03 +0100

    Update AlanineMutagenesis.py
    Added example

--- AlanineMutagenesis.py
+++ AlanineMutagenesis.py
@@ -1,14 +1,14 @@
-## run through pymol
+#run through pymol
 
 from pymol import cmd
-load file.pdb
+load 4ENX.pdb
 cmd.wizard("mutagenesis")
 cmd.refresh_wizard()
-cmd.get_wizard().do_select(("res xx"))
+cmd.get_wizard().do_select(("res 1"))
 cmd.get_wizard().set_mode('ALA')
 cmd.get_wizard().apply()
 cmd.set_wizard()
-cmd.save('file.pdb')
+cmd.save('ALAMUT-4ENX.pdb')
 reinitialize
 
 #cmd.get_wizard().set_mode('VAL') for ALA TO VAL
