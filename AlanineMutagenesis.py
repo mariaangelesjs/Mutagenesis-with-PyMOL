3de18897becad05010667e555598dc10f8db58ca
mariaangelesjs <mariaangelesjs@gmail.com>
2019-08-29 12:26:02 +0100

    updated res

--- AlanineMutagenesis.py
+++ AlanineMutagenesis.py
@@ -4,7 +4,7 @@ from pymol import cmd
 load file.pdb
 cmd.wizard("mutagenesis")
 cmd.refresh_wizard()
-cmd.get_wizard().do_select(("res 24"))
+cmd.get_wizard().do_select(("res xx"))
 cmd.get_wizard().set_mode('ALA')
 cmd.get_wizard().apply()
 cmd.set_wizard()
