20e203b0a239dda46c69410613c2136f157b51b3
mariaangelesjs <mariaangelesjs@gmail.com>
2020-02-04 14:07:41 +0000

    Update AlanineMutagenesis.py

--- AlanineMutagenesis.py
+++ AlanineMutagenesis.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import pymol
+#start here if you are running this code in pymol
 from pymol import cmd
-load 4ENX.pdb
+cmd.load 4ENX.pdb
 cmd.wizard("mutagenesis")
 cmd.refresh_wizard()
 cmd.get_wizard().do_select(("res 1"))
@@ -11,7 +12,7 @@ cmd.get_wizard().apply()
 cmd.set_wizard()
 cmd.save('ALAMUT-4ENX.pdb')
 reinitialize
-#load "/PATH-TO-FILE/FILE.pdb"
+#cmd.load "/PATH-TO-FILE/FILE.pdb"
 #cmd.get_wizard().do_select(("res X")) X can be any residue present in the protein
 #cmd.get_wizard().set_mode('VAL') for ALA TO VAL
 #cmd.get_wizard().set_mode('ALA') for any/not ALA to ALA
